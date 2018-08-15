9b583a60aefcfc64a6adf4b6d47ef833d6e9d961
Camilo <camilo.torres-t@mail.escuelaing.edu.co>
2018-08-15 18:46:05 -0500

    primer commit avance

---
+++ angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit-page',
+  templateUrl: './task-edit-page.component.html',
+  styleUrls: ['./task-edit-page.component.css']
+})
+export class TaskEditPageComponent implements OnInit {
+  constructor(
+  ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ angular-seed/src/app/services/todo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TodoService {
+
+    private todos: Todo[] = [
+        new Todo('todo 1'),
+        new Todo('todo 2', 1, true),
+        new Todo('todo 3')
+    ];
+
+    constructor() { }
+    
+    list(): Todo[] {
+        return this.todos;
+    }
+
+}
